829ecb89a43c206d339cb4602977222a095784cb
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-14 23:34:22 -0400

    game is done from the inside

--- app.js
+++ app.js
@@ -11,22 +11,31 @@ class Ship{
         console.log(`this is the accuracy ${this.accuracy}`);
     }
     attacks(){
-        let remainingHP = currentDefnder.hull - currentPlayer.firepower;
-        currentDefnder.hull = remainingHP
-        console.log(`remainingHP of player ${currentDefnder.name} is: ${remainingHP}`);
+        let RandomAccuracy = Math.floor(Math.random() * (7.5 - 6 + 1) + 6);
+        if (RandomAccuracy <= currentPlayer.accuracy) {
+            let remainingHP = currentDefnder.hull - currentPlayer.firepower;
+            currentDefnder.hull = remainingHP
+            console.log(`you got a hit ${currentPlayer.name} accuracy is ${RandomAccuracy}`)
+            console.log(`remainingHP of player ${currentDefnder.name} is: ${remainingHP}`);
+         } else {
+            let remainingHP = currentDefnder.hull
+            console.log(`do you need glases ${currentPlayer.name} you missed:${currentDefnder.name} his HP is : ${remainingHP}`);
+            console.log(`your accuracy is${currentPlayer.accuracy} you need at lest ${RandomAccuracy}`)
+
+        }
     }
 }
-const USSAssembly = new Ship("USS Ship",20,3,.7);
+const USSAssembly = new Ship("USS Ship",20,3,7);
 // USSAssembly.stats();
 
 let alienHull = Math.floor(Math.random() * (6 - 3 + 1)) + 3;
 let alienFirepower = Math.floor(Math.random() * (4 - 2 + 1)) + 2;
-let alienAeccuracy = Math.random() * (0.8 - 0.6) + 0.6;
+let alienAeccuracy = Math.random() * (8 - 6 + 1) + 6;
 
 function getAlien() {
     alienHull = Math.floor(Math.random() * (6 - 3 + 1)) + 3;
     alienFirepower = Math.floor(Math.random() * (4 - 2 + 1)) + 2;
-    alienAeccuracy = Math.random() * (0.8 - 0.6) + 0.6;
+    alienAeccuracy = Math.random() * (8 - 6 + 1) + 6;
 
 }
 const alienShip = new Ship('alienShip',alienHull,alienFirepower,alienAeccuracy);              
@@ -49,9 +58,9 @@ function round(i) {
         playerTurn() 
         currentPlayer.attacks();
         alienShip.firepower = Math.floor(Math.random() * (4 - 2 + 1)) + 2;
-        alienShip.accuracy = Math.random() * (0.8 - 0.6) + 0.6;    
+        alienShip.accuracy = Math.random() * (8 - 6 + 1) + 6;    
     }
-    console.log(`the winer in round ${i+1} is ${currentPlayer.name}`);
+    console.log(`the winer in round ${i+1} is ${currentPlayer.name} HP ${currentPlayer.hull}`);
     getAlien();
     alienShip.hull =  Math.floor(Math.random() * (6 - 3 + 1)) + 3;
 }
@@ -60,9 +69,10 @@ function game() {
     for (let i = 0; i < 6; i++) {
         if (USSAssembly.hull >= 0) {
             round(i);
-            console.log(`-----------------------------`)
+            console.log(`-----------------------------`);
+            
         }else{
-            console.log(`you lost on round${i+1}`)
+            console.log(`you lost on round: ${i+1}`)
         }
         
 
@@ -80,24 +90,3 @@ function game() {
 //      If you retreat, the game is over, perhaps leaving the game open for further developments or options
 //      You win the game if you destroy all of the aliens
 //      You lose the game if you are destroyed
-
-
-// function attacks(attacker,defnder) {
-//     let remainingHP = defnder - attacker;
-//     return remainingHP
-// }
-
-
-
-
-
-
-
-
-
-
-// The alien ships should each have the following ranged properties determined randomly:
-
-// hull - between 3and 6
-// firepower - between 2and 4
-// accuracy - between .6and .8
